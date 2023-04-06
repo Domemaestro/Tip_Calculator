73b7742bc528b8264e608100ddc91fe7ded0ce32
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-06 17:33:22 +0530

    commit

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -4,27 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    val billAmount = findViewById<EditText>(R.id.billEt)
-    val tipPercent = findViewById<EditText>(R.id.tipEt)
-    val splitNo = findViewById<EditText>(R.id.splitBillEt)
-    val calBtn = findViewById<Button>(R.id.calBtn)
-    val clearBtn = findViewById<Button>(R.id.clearBtn)
+    private val billAmount = findViewById<EditText>(R.id.billEt)
+    private val tipPercent = findViewById<EditText>(R.id.tipEt)
+    private val splitNo = findViewById<EditText>(R.id.splitBillEt)
+    private val calBtn = findViewById<Button>(R.id.calBtn)
+    private val clearBtn = findViewById<Button>(R.id.clearBtn)
+    private val displayResult = findViewById<TextView>(R.id.displayTv)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         calBtn.setOnClickListener {
             calculate()
+        }
+        clearBtn.setOnClickListener {
             clear()
         }
 
     }
 
     private fun calculate(){
+        val stringInput = billAmount.text.toString()
+        val cost = stringInput.toDouble()
+
+        val stringInput2 = tipPercent.text.toString()
+        val tipPercen = stringInput2.toInt()
+
+        val tipResult = cost * tipPercen
+        displayResult.text = tipResult.toString()
+
 
     }
     private fun clear(){
