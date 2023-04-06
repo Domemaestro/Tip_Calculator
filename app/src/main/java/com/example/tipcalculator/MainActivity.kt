06df72b601370e1d9f92ffa5b8bd71846cb8315c
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-06 17:17:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tipcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    val billAmount = findViewById<EditText>(R.id.billEt)
+    val tipPercent = findViewById<EditText>(R.id.tipEt)
+    val splitNo = findViewById<EditText>(R.id.splitBillEt)
+    val calBtn = findViewById<Button>(R.id.calBtn)
+    val clearBtn = findViewById<Button>(R.id.clearBtn)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        calBtn.setOnClickListener {
+            calculate()
+        }
+
+    }
+
+    private fun calculate(){
+
+    }
+
+}
